160059313eb551a35f9f31ef0b3d2aca2b86d9f5
John Gibbs <johngibbs110@gmail.com>
2020-07-06 18:10:02 -0500

    Work on assignment four.

--- queries.sql
+++ queries.sql
@@ -1,7 +1,19 @@
 ## Part 1: Test it with SQL
+id: int
+employer: varchar
+name: varchar
+skills: varchar
 
 ## Part 2: Test it with SQL
+SELECT *
+FROM employer
+WHERE location = "Saint Louis City"
 
 ## Part 3: Test it with SQL
+DROP TABLE job
 
 ## Part 4: Test it with SQL
+SELECT name
+FROM techjobs.skill
+INNER JOIN techjobs.job_skills ON job_skills.skills_id = skill.id
+ORDER BY name ASC;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,35 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @ManyToMany(mappedBy="skills")
+    private List<Job> jobs = new ArrayList<>();
+
+    @NotBlank
+    private String description;
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public Skill () {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
 }
